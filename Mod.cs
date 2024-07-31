ce6cb693a62afc80e92ae75df3cfbfb4cde04022
LiquidScammer <liquidscamer@gmail.com>
2024-07-31 16:46:24 -0700

    Add files via upload

---
+++ Mod.cs
@@ -0,0 +1,67 @@
+﻿using BepInEx;
+using $safeprojectname$.Patches;
+using System;
+using UnityEngine;
+using Utilla;
+
+namespace $safeprojectname$
+{
+    /// <summary>
+    /// This is your mod's main class.
+    /// </summary>
+
+    /* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin("com.author.gorillatag.modname", "ModName", "1.0.0")]
+    public class Mod : BaseUnityPlugin
+    {
+        bool inRoom;
+
+        void Start()
+        {
+            /* A lot of Gorilla Tag systems will not be set up when start is called /*
+			/* Put code in OnGameInitialized to avoid null references */
+
+            Utilla.Events.GameInitialized += OnGameInitialized;
+        }
+
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            /* Code here runs after the game initializes (i.e. GorillaLocomotion.Player.Instance != null) */
+        }
+
+        void Update()
+        {
+            if(inRoom)
+            {
+                //ModName();
+            }
+        }
+
+        /* This attribute tells Utilla to call this method when a modded room is joined */
+        [ModdedGamemodeJoin]
+        public void OnJoin(string gamemode)
+        {
+            //Activate your mod here
+            //ModName();
+
+            inRoom = true;
+        }
+
+        /* This attribute tells Utilla to call this method when a modded room is left */
+        [ModdedGamemodeLeave]
+        public void OnLeave(string gamemode)
+        {
+            //Deactivate your mod here
+            //!ModName();
+
+            inRoom = false;
+        }
+
+        public void ModName()
+        {
+
+        }
+    }
+}
